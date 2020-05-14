8e5ec0360d3668f1a8047fa051b515de17f1732f
Kristaps Enkuzens <kristaps@kecom.lv>
2020-05-14 10:20:43 +0300

    added vue cli plugin, icons

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,7 @@
 module.exports = {
   pluginOptions: {
-    s3Deploy: {
-      registry: undefined,
-      awsProfile: 's3bandplanyl2',
-      region: 'eu-west-1',
-      bucket: 'bandplan.yl2.lv',
-      createBucket: false,
-      staticHosting: true,
-      assetPath: 'dist',
-      assetMatch: '**',
-      deployPath: '/',
-      acl: 'public-read',
-      pwa: false,
-      enableCloudfront: false,
-      uploadConcurrency: 5,
-      pluginVersion: '3.0.0',
-      staticIndexPage: 'index.html',
-      staticErrorPage: 'index.html'
+    pwa: {
+      themeColor: '#ffc000'
     }
   }
 }
