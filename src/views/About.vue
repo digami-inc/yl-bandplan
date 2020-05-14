26b89d6418a2575a4a7774f6bc8d12b64a50f376
Kristaps Enkuzens <kristaps@kecom.lv>
2020-05-14 09:58:57 +0300

    Version

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,10 +5,10 @@
       <p>Programma ir izstrādes stadijā un dati var būt kļūdaini.</p>
       <p>Dati ņemti no:</p>
       <ul>
-          <li><a href="https://www.iaru-r1.org/reference/band-plans/">IARU R1 Band Plans (www.iaru-r1.org)</a></li>
-          <li><a href="https://likumi.lv/ta/id/284085-radioamatieru-radiostaciju-buvesanas-ierikosanas-un-lietosanas-ka-ari-radioamatieru-apliecibas-sanemsanas-kartiba">Radioamatieru radiostaciju būvēšanas, ierīkošanas un lietošanas, kā arī radioamatieru apliecības saņemšanas kārtība (likumi.lv)</a></li>
+          <li><a target="_blank" href="https://www.iaru-r1.org/reference/band-plans/">IARU R1 Band Plans (www.iaru-r1.org)</a></li>
+          <li><a target="_blank" href="https://likumi.lv/ta/id/284085-radioamatieru-radiostaciju-buvesanas-ierikosanas-un-lietosanas-ka-ari-radioamatieru-apliecibas-sanemsanas-kartiba">Radioamatieru radiostaciju būvēšanas, ierīkošanas un lietošanas, kā arī radioamatieru apliecības saņemšanas kārtība (likumi.lv)</a></li>
       </ul>
-      <p>Author: YL3AME Kristaps <a href="https://ame.yl3.lv/">ame.yl3.lv</a></p>
+      <p>Author: YL3AME Kristaps <a target="_blank" href="https://ame.yl3.lv/">ame.yl3.lv</a></p>
       <h2>Kas Jauns</h2>
       <p>2020-05-13 Pabeigti MK noteikumi 2. pielikums. Izlabots IARU tabulas krāsas 40m. Atvērta kartiņa aizveras pārslēdzot privilēģijas.</p>
       <p>2020-05-12 Uzlabots izskats. Pabeigtas visu HF joslu IARU un MK noteikumu tabulas.</p>
